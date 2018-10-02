6715da310faedd2c9ababcf2a6a7bf6d11dbf073
Divyanshu Dhruw <divyanshudhruw@gmail.com>
2018-10-02 09:30:19 +0530

    Setup Node Server Response System and Axios Request

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+	constructor(){
+		super();
+
+		this.state = {
+			bookName: "",
+		}
+	}
+
+	updateBookName = (event) => {
+		this.setState({ bookName: event.target.value })
+	}
+
+	searchBooks = () => {
+		axios.get('http://localhost:3001/search/' + this.state.bookName )
+		.then(response => {
+			console.log(response)
+		})
+		.catch(error => {
+
+		})
+	}
+
+	render() {
+		return (
+			<div>
+				<input type="text" onChange={this.updateBookName} />
+				<button onClick={this.searchBooks} > Search </button>
+			</div>
+		);
+	}
 }
 
 export default App;
